3230658abf7f31778cf17e811b810e024cd5561c
Harshit Agrawal <madhavtwister@gmail.com>
2018-05-16 12:26:42 +0530

    Initial commit

---
+++ app/src/main/java/com/ateam/funshoppers/MyFirebaseMessagingService.java
@@ -0,0 +1,58 @@
+package com.ateam.funshoppers;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Home on 1/29/2017.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private String TAG=FirebaseMessagingService.class.getName();
+    String message="";
+    String title="";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+
+        // ...
+
+        Log.i(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.i(TAG, "Message data payload: " + remoteMessage.getData().get("message"));
+            message = remoteMessage.getData().get("message");
+            //title=remoteMessage.getData().get("title");}
+        }
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.i(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+           // message=remoteMessage.getNotification().getBody();
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        final Intent emptyIntent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0, emptyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("New Message")
+                        .setContentText(message)
+                        .setContentIntent(pendingIntent);
+        mBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, mBuilder.build());
+    }
+
+}
